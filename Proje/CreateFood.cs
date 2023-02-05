f019f7820556f9c63b04e6d9f403a8bd329ae66d
alevdenizakca <alevdenizakca@gmail.com>
2023-02-05 13:59:18 +0300

    CreateFood metodu oluşturuldu

--- Proje/CreateFood.cs
+++ Proje/CreateFood.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CalCalculatorBLL;
+using CalCalculatorEntities;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +17,17 @@ namespace Proje
         public CreateFood()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string foodName = txtFoodName.Text;
+            int foodCarb = Convert.ToInt32(txtCarbonh.Text);
+            int foodFat = Convert.ToInt32(txtFat.Text);
+            int foodProt = Convert.ToInt32(txtProt.Text);
+            int foodCal = Convert.ToInt32(txtCal.Text);
+            FoodServices foodServices = new FoodServices();
+            foodServices.CreateFood(foodName, foodCarb, foodFat, foodProt, foodCal);
         }
     }
 }
