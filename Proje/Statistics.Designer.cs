da465e3811b7068fafc4b7bfa69f174b5485b121
yusa tosun <yusa_t@hotmail.com>
2023-02-19 18:38:40 +0300

    Statistic Front ve back ayarları

--- Proje/Statistics.Designer.cs
+++ Proje/Statistics.Designer.cs
@@ -30,11 +30,7 @@
         {
             this.dtpStartDate = new System.Windows.Forms.DateTimePicker();
             this.dtpEndDate = new System.Windows.Forms.DateTimePicker();
-            this.rdnToplamCal = new System.Windows.Forms.RadioButton();
-            this.rdnCategories = new System.Windows.Forms.RadioButton();
-            this.btn_Getir = new System.Windows.Forms.Button();
             this.dgvStatisticsTable = new System.Windows.Forms.DataGridView();
-            this.rdnFiltre3 = new System.Windows.Forms.RadioButton();
             this.btnDailyReport = new System.Windows.Forms.Button();
             this.btnMonthlyReport = new System.Windows.Forms.Button();
             this.btnWeeklyReport = new System.Windows.Forms.Button();
@@ -50,90 +46,34 @@
             // dtpStartDate
             // 
             this.dtpStartDate.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.dtpStartDate.Location = new System.Drawing.Point(16, 21);
-            this.dtpStartDate.Margin = new System.Windows.Forms.Padding(2);
+            this.dtpStartDate.Location = new System.Drawing.Point(23, 35);
             this.dtpStartDate.Name = "dtpStartDate";
-            this.dtpStartDate.Size = new System.Drawing.Size(240, 29);
+            this.dtpStartDate.Size = new System.Drawing.Size(341, 39);
             this.dtpStartDate.TabIndex = 0;
             // 
             // dtpEndDate
             // 
             this.dtpEndDate.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.dtpEndDate.Location = new System.Drawing.Point(364, 21);
-            this.dtpEndDate.Margin = new System.Windows.Forms.Padding(2);
+            this.dtpEndDate.Location = new System.Drawing.Point(520, 35);
             this.dtpEndDate.Name = "dtpEndDate";
-            this.dtpEndDate.Size = new System.Drawing.Size(245, 29);
+            this.dtpEndDate.Size = new System.Drawing.Size(348, 39);
             this.dtpEndDate.TabIndex = 0;
-            // 
-            // rdnToplamCal
-            // 
-            this.rdnToplamCal.AutoSize = true;
-            this.rdnToplamCal.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(199)))), ((int)(((byte)(199)))), ((int)(((byte)(199)))));
-            this.rdnToplamCal.Location = new System.Drawing.Point(39, 324);
-            this.rdnToplamCal.Margin = new System.Windows.Forms.Padding(2);
-            this.rdnToplamCal.Name = "rdnToplamCal";
-            this.rdnToplamCal.Size = new System.Drawing.Size(119, 19);
-            this.rdnToplamCal.TabIndex = 1;
-            this.rdnToplamCal.TabStop = true;
-            this.rdnToplamCal.Text = "Daily Total Calorie";
-            this.rdnToplamCal.UseVisualStyleBackColor = false;
-            this.rdnToplamCal.Visible = false;
-            // 
-            // rdnCategories
-            // 
-            this.rdnCategories.AutoSize = true;
-            this.rdnCategories.Location = new System.Drawing.Point(39, 369);
-            this.rdnCategories.Margin = new System.Windows.Forms.Padding(2);
-            this.rdnCategories.Name = "rdnCategories";
-            this.rdnCategories.Size = new System.Drawing.Size(81, 19);
-            this.rdnCategories.TabIndex = 1;
-            this.rdnCategories.TabStop = true;
-            this.rdnCategories.Text = "Categories";
-            this.rdnCategories.UseVisualStyleBackColor = true;
-            this.rdnCategories.Visible = false;
-            // 
-            // btn_Getir
-            // 
-            this.btn_Getir.Location = new System.Drawing.Point(32, 430);
-            this.btn_Getir.Margin = new System.Windows.Forms.Padding(2);
-            this.btn_Getir.Name = "btn_Getir";
-            this.btn_Getir.Size = new System.Drawing.Size(141, 29);
-            this.btn_Getir.TabIndex = 2;
-            this.btn_Getir.Text = "Show ";
-            this.btn_Getir.UseVisualStyleBackColor = true;
-            this.btn_Getir.Visible = false;
-            this.btn_Getir.Click += new System.EventHandler(this.btn_Getir_Click);
             // 
             // dgvStatisticsTable
             // 
             this.dgvStatisticsTable.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvStatisticsTable.Location = new System.Drawing.Point(220, 137);
-            this.dgvStatisticsTable.Margin = new System.Windows.Forms.Padding(2);
+            this.dgvStatisticsTable.Location = new System.Drawing.Point(218, 228);
             this.dgvStatisticsTable.Name = "dgvStatisticsTable";
             this.dgvStatisticsTable.RowHeadersWidth = 62;
             this.dgvStatisticsTable.RowTemplate.Height = 33;
-            this.dgvStatisticsTable.Size = new System.Drawing.Size(434, 322);
+            this.dgvStatisticsTable.Size = new System.Drawing.Size(716, 537);
             this.dgvStatisticsTable.TabIndex = 3;
-            // 
-            // rdnFiltre3
-            // 
-            this.rdnFiltre3.AutoSize = true;
-            this.rdnFiltre3.Location = new System.Drawing.Point(39, 283);
-            this.rdnFiltre3.Margin = new System.Windows.Forms.Padding(2);
-            this.rdnFiltre3.Name = "rdnFiltre3";
-            this.rdnFiltre3.Size = new System.Drawing.Size(141, 19);
-            this.rdnFiltre3.TabIndex = 6;
-            this.rdnFiltre3.TabStop = true;
-            this.rdnFiltre3.Text = "Total Cal For Category";
-            this.rdnFiltre3.UseVisualStyleBackColor = true;
-            this.rdnFiltre3.Visible = false;
             // 
             // btnDailyReport
             // 
-            this.btnDailyReport.Location = new System.Drawing.Point(27, 137);
-            this.btnDailyReport.Margin = new System.Windows.Forms.Padding(2);
+            this.btnDailyReport.Location = new System.Drawing.Point(39, 228);
             this.btnDailyReport.Name = "btnDailyReport";
-            this.btnDailyReport.Size = new System.Drawing.Size(105, 30);
+            this.btnDailyReport.Size = new System.Drawing.Size(150, 50);
             this.btnDailyReport.TabIndex = 7;
             this.btnDailyReport.Text = "Daily Report";
             this.btnDailyReport.UseVisualStyleBackColor = true;
@@ -141,32 +81,29 @@
             // 
             // btnMonthlyReport
             // 
-            this.btnMonthlyReport.Location = new System.Drawing.Point(27, 204);
-            this.btnMonthlyReport.Margin = new System.Windows.Forms.Padding(2);
+            this.btnMonthlyReport.Location = new System.Drawing.Point(16, 381);
             this.btnMonthlyReport.Name = "btnMonthlyReport";
-            this.btnMonthlyReport.Size = new System.Drawing.Size(105, 30);
+            this.btnMonthlyReport.Size = new System.Drawing.Size(173, 50);
             this.btnMonthlyReport.TabIndex = 7;
-            this.btnMonthlyReport.Text = "Montly Report";
+            this.btnMonthlyReport.Text = "Montly Compare";
             this.btnMonthlyReport.UseVisualStyleBackColor = true;
             this.btnMonthlyReport.Click += new System.EventHandler(this.btnMonthlyReport_Click);
             // 
             // btnWeeklyReport
             // 
-            this.btnWeeklyReport.Location = new System.Drawing.Point(27, 171);
-            this.btnWeeklyReport.Margin = new System.Windows.Forms.Padding(2);
+            this.btnWeeklyReport.Location = new System.Drawing.Point(16, 326);
             this.btnWeeklyReport.Name = "btnWeeklyReport";
-            this.btnWeeklyReport.Size = new System.Drawing.Size(105, 30);
+            this.btnWeeklyReport.Size = new System.Drawing.Size(173, 50);
             this.btnWeeklyReport.TabIndex = 8;
-            this.btnWeeklyReport.Text = "Weekly Report";
+            this.btnWeeklyReport.Text = "Weekly Compare";
             this.btnWeeklyReport.UseVisualStyleBackColor = true;
             this.btnWeeklyReport.Click += new System.EventHandler(this.btnWeeklyReport_Click);
             // 
             // btnSendMail
             // 
-            this.btnSendMail.Location = new System.Drawing.Point(526, 479);
-            this.btnSendMail.Margin = new System.Windows.Forms.Padding(2);
+            this.btnSendMail.Location = new System.Drawing.Point(751, 798);
             this.btnSendMail.Name = "btnSendMail";
-            this.btnSendMail.Size = new System.Drawing.Size(128, 24);
+            this.btnSendMail.Size = new System.Drawing.Size(183, 40);
             this.btnSendMail.TabIndex = 9;
             this.btnSendMail.Text = "Send Mail to Me";
             this.btnSendMail.UseVisualStyleBackColor = true;
@@ -185,11 +122,13 @@
             this.btnDailyTotalCalorie.IconFont = FontAwesome.Sharp.IconFont.Auto;
             this.btnDailyTotalCalorie.IconSize = 24;
             this.btnDailyTotalCalorie.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnDailyTotalCalorie.Location = new System.Drawing.Point(16, 64);
+            this.btnDailyTotalCalorie.Location = new System.Drawing.Point(23, 107);
+            this.btnDailyTotalCalorie.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnDailyTotalCalorie.Name = "btnDailyTotalCalorie";
-            this.btnDailyTotalCalorie.Size = new System.Drawing.Size(170, 35);
+            this.btnDailyTotalCalorie.Size = new System.Drawing.Size(222, 58);
             this.btnDailyTotalCalorie.TabIndex = 33;
-            this.btnDailyTotalCalorie.Text = "       Daily Total Calorie";
+            this.btnDailyTotalCalorie.Text = "      Group By Day";
+            this.btnDailyTotalCalorie.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.btnDailyTotalCalorie.UseVisualStyleBackColor = false;
             this.btnDailyTotalCalorie.Click += new System.EventHandler(this.btnDailyTotalCalorie_Click);
             // 
@@ -200,12 +139,16 @@
             this.groupBox1.Controls.Add(this.dtpStartDate);
             this.groupBox1.Controls.Add(this.btnDailyTotalCalorie);
             this.groupBox1.Controls.Add(this.dtpEndDate);
-            this.groupBox1.Location = new System.Drawing.Point(27, 12);
+            this.groupBox1.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.groupBox1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.groupBox1.Location = new System.Drawing.Point(39, 20);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(627, 109);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox1.Size = new System.Drawing.Size(896, 182);
             this.groupBox1.TabIndex = 34;
             this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Query ";
+            this.groupBox1.Text = "Serach By Date Panel";
             // 
             // btnTotalCalForCategory
             // 
@@ -220,11 +163,12 @@
             this.btnTotalCalForCategory.IconFont = FontAwesome.Sharp.IconFont.Auto;
             this.btnTotalCalForCategory.IconSize = 32;
             this.btnTotalCalForCategory.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnTotalCalForCategory.Location = new System.Drawing.Point(409, 64);
+            this.btnTotalCalForCategory.Location = new System.Drawing.Point(570, 107);
+            this.btnTotalCalForCategory.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnTotalCalForCategory.Name = "btnTotalCalForCategory";
-            this.btnTotalCalForCategory.Size = new System.Drawing.Size(200, 35);
+            this.btnTotalCalForCategory.Size = new System.Drawing.Size(300, 58);
             this.btnTotalCalForCategory.TabIndex = 35;
-            this.btnTotalCalForCategory.Text = "       Total Cal For Category";
+            this.btnTotalCalForCategory.Text = "Group By Total Cal";
             this.btnTotalCalForCategory.UseVisualStyleBackColor = false;
             this.btnTotalCalForCategory.Click += new System.EventHandler(this.btnTotalCalForCategory_Click);
             // 
@@ -241,38 +185,35 @@
             this.btnCategories.IconFont = FontAwesome.Sharp.IconFont.Auto;
             this.btnCategories.IconSize = 32;
             this.btnCategories.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnCategories.Location = new System.Drawing.Point(225, 64);
+            this.btnCategories.Location = new System.Drawing.Point(264, 107);
+            this.btnCategories.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnCategories.Name = "btnCategories";
-            this.btnCategories.Size = new System.Drawing.Size(161, 35);
+            this.btnCategories.Size = new System.Drawing.Size(284, 58);
             this.btnCategories.TabIndex = 34;
-            this.btnCategories.Text = "    Categories";
+            this.btnCategories.Text = "Group By Category";
             this.btnCategories.UseVisualStyleBackColor = false;
             this.btnCategories.Click += new System.EventHandler(this.btnCategories_Click);
             // 
             // Statistics
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(54)))), ((int)(((byte)(60)))), ((int)(((byte)(73)))));
-            this.ClientSize = new System.Drawing.Size(674, 521);
+            this.ClientSize = new System.Drawing.Size(963, 868);
             this.Controls.Add(this.groupBox1);
-            this.Controls.Add(this.rdnToplamCal);
-            this.Controls.Add(this.rdnCategories);
             this.Controls.Add(this.btnSendMail);
-            this.Controls.Add(this.rdnFiltre3);
             this.Controls.Add(this.btnWeeklyReport);
             this.Controls.Add(this.btnMonthlyReport);
             this.Controls.Add(this.btnDailyReport);
             this.Controls.Add(this.dgvStatisticsTable);
-            this.Controls.Add(this.btn_Getir);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Statistics";
             this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
             this.Text = "Statistics";
             ((System.ComponentModel.ISupportInitialize)(this.dgvStatisticsTable)).EndInit();
             this.groupBox1.ResumeLayout(false);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -280,11 +221,7 @@
 
         private DateTimePicker dtpStartDate;
         private DateTimePicker dtpEndDate;
-        private RadioButton rdnToplamCal;
-        private RadioButton rdnCategories;
-        private Button btn_Getir;
         private DataGridView dgvStatisticsTable;
-        private RadioButton rdnFiltre3;
         private Button btnDailyReport;
         private Button btnMonthlyReport;
         private Button btnWeeklyReport;
